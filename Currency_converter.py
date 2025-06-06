cf00fdd58cc01a3d6922b23aab0ef147d0712648
SHIVAM SHARMA <164771907+Shivamsharma9425@users.noreply.github.com>
2025-06-06 12:28:12 +0530

    Add files via upload

---
+++ Currency_converter.py
@@ -0,0 +1,24 @@
+
+#importing necessary libraries 
+import streamlit as st
+import requests
+
+#Set title
+st.title("Live Currency Converter")
+
+#Taking inputs fro user
+amount = st.number_input("Enter the amount in INR: ", 1)
+target_curr = st.selectbox("Convert to: ", ["USD", "JPY","EUR"])
+
+#fetching and displaying data
+if st.button("Convert"):
+    url = "https://api.exchangerate-api.com/v4/latest/INR"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        rate = data["rates"][target_curr]
+        converted = amount* rate
+        st.success(f"{amount} INR= {converted} {target_curr}")
+    else:
+        st.error("Some Error Occured")
